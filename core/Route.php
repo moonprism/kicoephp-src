55a82cda0f19cc7b90e44502a332546010662c5f
moonprism <ankicoe@gmail.com>
2020-10-16 23:29:38 +0800

    init 2.0 & Route

--- core/Route.php
+++ core/Route.php
@@ -1,203 +1,365 @@
 <?php
-// 简单的路由处理类
 
 namespace kicoe\core;
 
-use \ReflectionClass;
-use kicoe\core\cache\Factory as CacheFactory;
-
-class Route{
-
-    // 中间件列表
-    // 对了，用swoole载入内存后这一块全要改，当初写的什么蠢代码
-    private static $middleware_list = [];
-
-    // 控制器
-    private static $controller = 'Index';
-    // 操作
-    private static $action = 'index';
-    // 参数
-    private static $query = [];
+class Route
+{
+    /**
+     * 超快速的路由前缀树实现
+     * example:
+     * 'GET' => [
+     *   'path' => '/'
+     *   'handler' => [],
+     *   'children' => [
+     *     'i' => [
+     *       'path' => 'index/',
+     *       'handler' => [],
+     *       'children' => [
+     *         '$' => [
+     *           'path' => 'var1',
+     *           'handler' => ['controller', 'method'],
+     *           'children' => [],
+     *         ],
+     *         't' => [
+     *           'path' => 'test',
+     *           'handler' => ['controller2', 'method2'],
+     *           'children' => [],
+     *         ]
+     *       ]
+     *     ]
+     *   ]
+     *   ...
+     */
+    public static array $tree = [
+        'GET' => [],
+        'POST' => [],
+        'PUT' => [],
+        'DELETE' => [],
+    ];
 
     /**
-     * 获取控制器名
-     * @return string 控制器名
+     * 从控制器方法注解中解析路由
+     * @param $class_name
      */
-    public static function getController()
+    public static function parseAnnotation($class_name)
     {
-        return self::$controller;
-    }
+        try {
+            $class = new \ReflectionClass($class_name);
+        } catch (\ReflectionException $e) {
+            throw new \InvalidArgumentException(sprintf('Class "%s" does not exist.', $class_name));
+        }
 
-    /**
-     * 获取操作名
-     * @return string 操作
-     */
-    public static function getAction()
-    {
-        return self::$action;
-    }
-
-    /**
-     * 初始化路由
-     * @param string $url ?k=
-     * @throws
-     */
-    public static function init($url)
-    {
-
-        $route_conf = Config::prpr('route');
-        $cache = CacheFactory::getInstance(ucfirst(Config::prpr('route_cache')));
-        $url = trim($url,'/');
-        if (!$url) {
-            // 路由为空则查找'/'路由
-            if (!isset($route_conf['/'])) {
-                return;
+        foreach ($class->getMethods() as $method) {
+            if ($doc = $method->getDocComment()) {
+                // @route get /xx/xx
+                preg_match_all('/@route (.+?) (.+?)\s/', $doc, $matches, PREG_SET_ORDER);
+                foreach ($matches as $mat) {
+                    self::add($mat[1], $mat[2], [$class_name, $method->getName()]);
+                }
             }
-            $def_route = explode('/', $route_conf['/']);
-            self::$controller = $def_route[0];
-            self::$action = $def_route[1];
+        }
+    }
+
+    /**
+     * @param string $type GET or POST...
+     * @param string $path
+     * @param $handler
+     */
+    protected static function addRoute(string $type, string $path, $handler)
+    {
+        $type = strtoupper($type);
+        $res = self::$tree[$type];
+        if ($res === []) {
+            $res = self::$tree[$type] = self::generateTreeNode('/', [], []);
+        }
+        $str = trim($path, '/');
+        $stack = [$type];
+        // init
+        if ($path === '/') {
+            self::$tree[$type]['handler'] = $handler;
             return;
         }
-        if ($route_conf === []) {
-            // 配置为空则自动路由
-            $k_arr = explode('/', $url);
-            self::$controller = $k_arr[0];
-            self::$action = $k_arr[1];
-            if ( isset($k_arr[2]) ) {
-                self::$query = array_slice($k_arr,2);
-            }
-        } else {
-            // 完全按照配置来
-            $key = 'route';
-            if (!Config::prpr('test') && $cache->has($key)) {
-                $route_cache = $cache->read($key);
-            } else {
-                $route_cache = self::tree($route_conf);
-                $cache->write($key, $route_cache);
-            }
-            // 解析路由
-            $url_arr = explode('/', $url);
-            $offset = 0;
-            foreach ($url_arr as $i => $key) {
-                $offset = $i;
-                $route_cache = &$route_cache[$key];
-                if ($i === count($url_arr)-1 || isset($route_cache['/']) && !isset($route_cache[$url_arr[$i+1]])) {
-                    $route_cache = &$route_cache['/'];
+        while ($res['children'] !== [] && $str !== '') {
+            $start_char = $str[0];
+            // 解析栈，路由出问题从这里开始排查
+            $stack[] = $start_char !== '{' ? $start_char : '$';
+            if ($node = $res['children'][$start_char] ?? false) {
+                $path_len = strlen($node['path']);
+                if (substr($str, 0, $path_len) === $node['path']) {
+                    $res = $node;
+                    $str = substr($str, $path_len);
+                    continue;
+                } else {
+                    // LCP
+                    $sl = strlen($str);
+                    $pl = strlen($node['path']);
+                    for ($l = 0; $l < min($sl, $pl) && $str[$l] === $node['path'][$l]; $l++){};
+                    $lcp_str = substr($str, 0, $l);
+                    // 分裂构造node
+                    $str = substr($str, $l);
+                    $p_str = substr($node['path'], $l);
+                    $node['path'] = $p_str;
+                    $attach_node = self::generateTreeNode($lcp_str, [], [$p_str[0] => $node]);
+                    if ($sl === $l) {
+                        // 将分裂节点作为新node的最终目的
+                        $attach_node['handler'] = $handler;
+                    } else {
+                        // 两开花
+                        $parse_node = self::parseTreeNode($str, $handler);
+                        $attach_node['children'][$parse_node[0]] = $parse_node[1];
+                    }
+                    $str = '';
+                    self::setTreeNodeByCallStack($attach_node, $stack);
                     break;
                 }
+            } else {
+                if ($node = $res['children']['$'] ?? false) {
+                    if ($start_char === '{') {
+                        // 变量直接匹配
+                        $var_end_pos = strpos($str, '}');
+                        if (!$var_end_pos) {
+                            throw new \InvalidArgumentException(sprintf('Route to "%s": variables definition not end', $path));
+                        }
+                        $res = $node;
+                        $str = substr($str, $var_end_pos+1);
+                        continue;
+                    }
+                }
+                // 生成一般节点直接附加
+                $parse_node = self::parseTreeNode($str, $handler);
+                $child_node = $parse_node[1];
+                self::setTreeNodeByCallStack($child_node, $stack);
+                $str = '';
+                break;
             }
-            if (!isset($route_cache[0])) {
-                throw new Exception('route no find', $url);
-            }
-            if (isset($route_cache[2])) {
-                self::$middleware_list = $route_cache[2];
-            }
-            self::$controller = $route_cache[0];
-            self::$action = $route_cache[1];
-            // 解析参数
-            if ($qu_arr = array_slice($url_arr, $offset+1)) {
-                self::$query = $qu_arr;
-            }
+        }
+        if ($str !== '') {
+            $child_node = self::parseTreeNode($str, $handler);
+            $node['children'][$child_node[0]] = $child_node[1];
+            self::setTreeNodeByCallStack($node, $stack);
         }
     }
 
     /**
-     * 将路由配置解析为树, 类似前缀树，性能还是可以了
-     * @param array $conf 配置数组
-     * @return array 转换树结构
-     * @throws
+     * @param array $node
+     * @param array $stack 调用栈 eg:['GET', 'i', '/', '$']
      */
-    public static function tree($conf)
+    protected static function setTreeNodeByCallStack(array $node, array $stack)
     {
-        $tree = [];
-        $in_node = &$tree;
-        foreach ($conf as $key => $value) {
-            // 中间件列表
-            $mi_list = explode('|', $value);
-            $value = array_pop($mi_list);
-            $ca_list = explode('@', $value);
-            if (!isset($ca_list[1])) {
-                $ca_list[1] = 'index';
-            }
-            if ($mi_list) {
-                $ca_list[2] = $mi_list;
-            }
-            if (count($ca_list)<2) {
-                throw new Exception('route config export error', $value);
-            }
-            $route_arr = explode('/', trim($key, '/'));
-            foreach ($route_arr as $v) {
-                if (isset($in_node[$v])) {
-                    $in_node = &$in_node[$v];
-                } else {
-                    $in_node[$v] = [];
-                    $in_node = &$in_node[$v];
-                }
-            }
-            $in_node['/'] = $ca_list;
-            $in_node = &$tree;
+        $type = $stack[0];
+        if (count($stack) === 1) {
+            self::$tree[$type]['children'] = $node['children'];
+            return;
         }
-        
-        return $tree;
+        $node_point = &self::$tree[$type];
+        for ($i = 1; $i < count($stack); $i++) {
+            $node_point = &$node_point['children'][$stack[$i]];
+        }
+        $node_point = $node;
     }
 
     /**
-     * 利用反射检查
-     * @throws
+     * 一般线性节点的生成
+     * @param string $path
+     * @param $handler
+     * @return array [ 'k', node ]
      */
-    public static function run()
+    protected static function parseTreeNode(string $path, $handler)
     {
-        $controller = 'app\controller\\'. ucfirst(self::$controller);
-        $action = self::$action;
-
-        // 验证中间件
-        if (self::$middleware_list) {
-            $middleware_class_name = Config::prpr('middleware');
-            if (!class_exists($middleware_class_name)) {
-                throw new Exception('middleware', "{$middleware_class_name} class is not exist");
+        preg_match_all('/({[a-zA-Z_][a-zA-Z0-9_-]*})/', $path, $matches, PREG_OFFSET_CAPTURE);
+        $path_stack = [];
+        $str_index = 0;
+        foreach ($matches[1] as $param) {
+            $str = substr($path, $str_index, $param[1] - $str_index);
+            if ($str !== '') {
+                $path_stack[] = [$str[0], $str];
             }
-            $middleware = new $middleware_class_name();
-            foreach (self::$middleware_list as $mw) {
-                if (!method_exists($middleware, $mw)) {
-                    return;
-                }
-                if (!$middleware->$mw()) {
-                    throw new Exception('middleware', "{$mw} not pass");
-                }
+            $path_stack[] = ['$', substr($path, $param[1]+1, strlen($param[0])-2)];
+            $str_index = $param[1] + strlen($param[0]);
+            if (strlen($path) > $str_index && substr($path, $str_index, 1) !== '/') {
+                throw new \InvalidArgumentException(sprintf('Route to "%s": variables must split by "/"', $path));
             }
         }
-        // 获取控制器类的反射实例
-        $controller_ref = new ReflectionClass($controller);
-        if ($controller_ref->isSubclassOf('kicoe\Core\Controller')) {
-            $action_ref = $controller_ref->getMethod(self::$action);
-            $params = $action_ref->getParameters();
-            $i = 0;
-            $pas = [];
-            // 注入[Request\Respinse\路由参数]依赖
-            foreach ($params as $pa) {
-                if ($class = $pa->getClass()) {
-                    if ('kicoe\core\Request' === $class->getName()) {
-                        $pas[] = Request::getInstance();
-                    } else if ('kicoe\core\Response' === $class->getName()) {
-                        $pas[] = Response::getInstance();
-                    }
-                } else {
-                    if (isset(self::$query[$i])) {
-                        $pas[] = self::$query[$i];
-                    }
-                    $i++;
+
+        if (strlen($path) > $str_index) {
+            $str = substr($path, $str_index);
+            $path_stack[] = [$str[0], $str];
+        }
+
+        $child_node = [];
+        $child_key = '';
+        for ($i = count($path_stack) - 1; $i >= 0; $i--) {
+            $node = self::generateTreeNode($path_stack[$i][1], [], []);
+            if ($child_key !== '') {
+                $node['children'][$child_key] = $child_node;
+            } else {
+                $node['handler'] = $handler;
+            }
+            $child_key = $path_stack[$i][0];
+            $child_node = $node;
+        }
+        return [($path[0] !== '{' ? $path[0] : '$'), $child_node];
+    }
+
+    /**
+     * @param string $path
+     * @param array $handler
+     * @param array $children
+     * @return array node
+     */
+    protected static function generateTreeNode(string $path, array $handler, array $children)
+    {
+        return compact('path', 'handler', 'children');
+    }
+
+    public static function get(string $path, $call)
+    {
+        self::add('GET',  $path, $call);
+    }
+
+    public static function post(string $path, $call)
+    {
+        self::add('POST',  $path, $call);
+    }
+
+    public static function put(string $path, $call)
+    {
+        self::add('PUT',  $path, $call);
+    }
+
+    public static function delete(string $path, $call)
+    {
+        self::add('DELETE',  $path, $call);
+    }
+
+    /**
+     * @param string $type
+     * @param string $path
+     * @param $call
+     */
+    public static function add(string $type, string $path, $call)
+    {
+        // 暂不支持闭包的路由定义
+        if (is_array($call)) {
+            list($class_name, $method_name) = $call;
+        } else if (is_string($call)) {
+            list($class_name, $method_name) = explode('@', $call);
+        } else if ($call instanceof \Closure) {
+            self::addRoute($type, $path, $call);
+            return;
+        }
+        try {
+            $ref_class = new \ReflectionClass($class_name);
+            $ref_method = $ref_class->getMethod($method_name);
+        } catch (\ReflectionException $e) {
+            throw new \InvalidArgumentException(sprintf('Class "%s" or Function "%s" does not exist.', $class_name, $method_name));
+        }
+        self::addRoute($type, $path, [$ref_class->getName(), $ref_method->getName()]);
+    }
+
+    /**
+     * @param $path
+     * @param string $type
+     * @return array 返回一个可供执行的 info
+     */
+    public static function search($path, $type = 'GET')
+    {
+        $res = self::$tree[$type];
+        $str = trim($path, '/');
+        $params = [];
+        //$stack = [];
+        while ($res['children'] !== [] && $str !== '') {
+            $start_char = $str[0];
+            if ($node = $res['children'][$start_char] ?? false) {
+                $path_len = strlen($node['path']);
+                if (substr($str, 0, $path_len) === $node['path']) {
+                    //$stack[] = $start_char;
+                    $res = $node;
+                    $str = substr($str, $path_len);
+                    continue;
                 }
             }
+            if ($node = $res['children']['$'] ?? false) {
+                // 暂定参数后只能接 /
+                $var_name = $node['path'];
+                if ($after_node = $node['children']['/'] ?? false) {
+                    $pos = strpos($str, '/');
+                    if ($pos) {
+                        $param = substr($str, 0, $pos);
+                        $after_pa = substr($str, $pos);
+                        $path_len = strlen($after_node['path']);
+                        if (substr($after_pa, 0, $path_len) === $after_node['path']) {
+                            // 截取实际传递的到 / 位置的参数
+                            // $stack[] = '$';
+                            // $stack[] = '/';
+                            $params[$var_name] = $param;
+                            $res = $after_node;
+                            $str = substr($after_pa, $path_len);
+                            continue;
+                        }
+                    }
+                }
+                // 将剩余部分作为参数传入
+                $params[$var_name] = $str;
+                // $stack[] = '$';
+                $res = $node;
+                $str = '';
+            }
+            break;
+        }
+        if ($res['handler'] !== [] && $str === '') {
+            return [
+                'handler' => $res['handler'],
+                'params' => $params
+            ];
+        }
+        return [];
+    }
 
-            // 不用反射执行
-            $result = call_user_func_array([new $controller, $action], $pas);
+    public static function searchAndExecute(...$p)
+    {
+        $result = self::search(...$p);
+        if ($result !== []) {
+            if (is_array($result['handler'])) {
+                $class_name = $result['handler'][0];
+                $method_name = $result['handler'][1];
+                try {
+                    $ref_class = new \ReflectionClass($class_name);
+                    $ref_method = $ref_class->getMethod($method_name);
+                } catch (\ReflectionException $e) {
+                }
+                self::generateCallParams($ref_method, $params);
+                $result = call_user_func_array([new $class_name, $method_name], $params);
+            } else {
+                try {
+                    $ref_method = new \ReflectionFunction($result['handler']);
+                } catch (\ReflectionException $e) {
+                }
+                self::generateCallParams($ref_method, $params);
+                $result = call_user_func_array($result['handler'], $params);
+            }
+            return $result;
+        }
+        return null;
+    }
 
-            if (is_string($result)) {
-                Response::getInstance()->send($result);
-            } else if (is_array($result)) {
-                Response::getInstance()->json($result);
+    protected static function generateCallParams(\ReflectionFunction $ref_method, &$params)
+    {
+        $params = [];
+        $ref_params = $ref_method->getParameters();
+        foreach ($ref_params as $rp) {
+            // 自动注入
+            if ($type = $rp->getType()) {
+                switch ($type->getName()) {
+                    case '':
+                        // $params[] = xx.getInstance();
+                        break;
+                }
+            } else {
+                if ($param = $result['params'][$rp->getName()] ?? false) {
+                    $params[] = $param;
+                }
             }
         }
     }
-
 }
